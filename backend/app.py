b32cd0215dd45a150951de4163599e92f5794577
WonDuJin <windyjan1@naver.com>
2022-11-15 00:38:42 +0900

    판다스 써보기 sql오류는 해결못함

--- backend/app.py
+++ backend/app.py
@@ -2,6 +2,7 @@ from flask import Flask, jsonify, request
 from model import dusickdb
 
 
+
 app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 @app.route('/companylist',methods=['POST','GET'])
