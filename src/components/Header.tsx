5643dc9643102178e817950f31f75a43fa58312d
unknown <geunwon1947@gmail.com>
2022-11-23 18:09:11 +0900

    스타일 컴포넌트 사용해서 헤더 크기 변경해봄

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,9 +1,19 @@
+import styled from "styled-components"
+
+const HeaderSet = styled.header`
+  width: 100%;
+  height: 127px;
+  border: 1px solid black;
+`;
+
 const Header = ()=>{
   return (
-    <header>
+    <>
+    <HeaderSet>
       <h2>This is header</h2>
-    </header>
-  )
-}
+    </HeaderSet>
+    </>
+  );
+};
 
 export default Header
