ccb585e97912bb937d9b9e12518d85d1d5c02211
WonDuJin <windyjan1@naver.com>
2022-11-24 13:08:12 +0900

    app.py, model.py에 주석을 달아 팀원들이 좀 더 수월하게 파악할 수 있도록 함

--- backend/app.py
+++ backend/app.py
@@ -5,11 +5,13 @@ app = Flask(__name__)
 app.config['JSON_AS_ASCII'] = False
 CORS(app,resources={r'*':{'origins':'http://localhost:3000'}},supports_credentials=True)
 
+#kospi or kosdak을 URL에 입력하면 이를 매개변수로 받아 조회하도록 설정
 @app.route('/<market>',methods=['GET'])
 def allSearch(market):
   data = DataRoute.markets(market)
   return jsonify(data)
 
+# 프론트에서 리스트에 출력된 회사 정보를 클릭시 회사의 이름을 전송받아 조회할 수 있도록 설정
 @app.route('/getnames',methods=['POST'])
 def getname():
   data= DataRoute.getname()
--- backend/model.py
+++ backend/model.py
@@ -5,20 +5,27 @@ from dbconnection import db_connection
 db = db_connection
 
 class DataRoute():
-  
+
+  # 라우터에서 매개변수를 사용하여 URL 주소창에 kospi or kosdak을 치면 companly의 WHERE절 조건으로 해당 값이 입력된다.
   def markets(market):
     cur = db.cursor(pymysql.cursors.DictCursor)
+
+    #company list에서 랜덤으로 코스피 or 코스닥의 28개 회사의 market, code name을 조회 
     cur.execute(f"SELECT market,code,name FROM CompanyList WHERE market = '{market}' ORDER BY RAND() LIMIT 28")
     res = cur.fetchall()
-    data_stack = list()
+    data_stack = list() # 빈 배열 생성
+
+    # SQL문을 반복문을 통하여 빈 배열에 값을 계속 추가 
     for i in range(len(res)):
       code = res[i]["code"]
       market = res[i]["market"]
+      # companylist에서 조회한 값을 가지고 문자 리터럴을 통하여 값을 넣고 각회사의 정보 테이블과 companylist의 테이블을 INNER JOIN 시킨다.
       cur.execute(f'SELECT companylist.code AS code,market,name,high,low,close,volume,day,ROUND((high+low)/2,1) AS mid, ROUND((((high+low)/2)*0.04),2) AS medomesu FROM {market}_{code}_d AS api INNER JOIN companylist ON companylist.code = api.code WHERE day BETWEEN date("2022-01-27") AND date("2022-01-28")+1 ORDER BY day DESC LIMIT 2')
       res2 = cur.fetchall()
       data_stack.append(res2)
     return data_stack
   
+  # 프론트에서 리스트에 출력된 회사 이름을 클릭시 해당 회사의 이름 값을 전송받아 WHERE절 조건으로 입력되게 하고, 입력된 회사의 정보를 가져오기 위한 것
   def getname():
     if request.method=='POST':
       datas = request.get_json()
