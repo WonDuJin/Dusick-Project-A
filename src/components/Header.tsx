4f8ba0ef0acdbc74c718c64de72391c0399d243b
unknown <geunwon1947@gmail.com>
2022-11-25 16:54:36 +0900

    Header, Layout에 useState 개념 공부해서 적용시킴

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,76 @@
+import React from "react";
 import styled from "styled-components"
 
 const HeaderSet = styled.header`
   width: 100%;
   height: 127px;
-  border: 1px solid black;
+  padding: 0 35px;
+  display: flex;
+  align-items:center;
+  justify-content: space-between;
+  border-bottom: 1px solid ${(props)=>props.theme.color.l_gray}
+  & > img {
+    width : 161px;
+    height: 66px;
+  }
+  & > div{
+    position: relative;
+    display: flex;
+    align-items: center;
+    & > span{
+      width: 11px;
+      height: 6.5px;
+      background: url('./assets/Direction.png') center/cover no-repeat;
+      position: absolute;
+      right: 42px;
+      top: 22px;
+    }
+    & > input[type='text'] {
+      background: ${(props)=> props.theme.color.white_gray};
+      border: 1px solid ${(props)=> props.theme.color.l_gray};
+      border-radius: 30px;
+      width: 463px;
+      height: 50px;
+    }
+    & > select {
+      width: 113px;
+      height: 50px;
+      margin-right: 0 15px;
+      border-radius: 30px;
+      color: ${(props)=>props.theme.color.gray};
+      font-szie: ${(props)=>props.theme.fontSize.font_18};
+      font-weight: ${(props)=>props.theme.fontWeight.DemiLight};
+      border: 1px solid ${(props)=>props.theme.color.m_gray};
+      appearance: none;
+      &:active,
+      &:focus {
+        outline: none;
+      }
+      & > option {
+        font-size: ${(props)=>props.theme.fontSize.font_18};
+        font-weight: ${(props)=>props.theme.fontWeight.DemiLight};
+      }
+    }
+  }
 `;
 
-const Header = ()=>{
+const Header = ({getStockType}:{getStockType:(Type:string)=>void})=>{
+  const Typeget = (e: React.ChangeEvent<HTMLSelectElement>) => {
+    getStockType(e.target.value);
+  };
+
   return (
     <>
     <HeaderSet>
-      <h2>This is header</h2>
-    </HeaderSet>
+        <img src="./assets/Logo.png" alt="logo"></img>
+        <div>
+          <select onChange={Typeget}>
+            <option value={'kospi'}>KOSPI</option>
+            <option value={'kosdak'}>KOSDAQ</option>
+          </select>
+          <span></span>
+        </div>
+      </HeaderSet>
     </>
   );
 };
--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,10 +1,19 @@
+import { useState } from "react"
 import Header from "./Header"
 import Section1 from "./Section1"
 
+
 const Layout = () =>{
+
+  const [name, setName] = useState('KOSPI');
+
+  const getStockType = (type:string) => {
+    setName(type)
+  }
+  console.log(name)
   return(
     <>
-      <Header />
+      <Header getStockType={getStockType}></Header>
       <div>
         <main>
 
